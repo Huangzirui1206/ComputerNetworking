683c7d99a81d904d1fe840f5a612b46781e12308
Huang Zirui <201220062@smail.nju.edu.cn>
2022-03-28 11:48:20 +0800

    Lab3 finished

--- myrouter.py
+++ myrouter.py
@@ -7,17 +7,78 @@ Basic IPv4 router (static routing) in Python.
 import time
 import switchyard
 from switchyard.lib.userlib import *
+import time
+
+class ARPCache(object):
+    def __init__(self, timeout = 180):
+        self.timeout = timeout
+        self.cache = {} # Ipaddr:[hwaddr,timestamp] 
+                        # If timestamp is negative, the entry is static
+                        # Otherwise it's dynamic.
+    
+    def put(self, key, value, timestamp = -1): # The default entry type is static
+        log_info(f"The cache puts in entry {key} -- {value}")
+        self.cache[key] = [value,timestamp]
+
+    def get(self, key):
+        if key in self.cache:
+            if self.cache[key][1] != -1:
+                self.cache[key][1] = time.time()
+            return self.cache[key][0]
+        else:
+            return None
+    
+    def refresh():  # Evict those dynamic entries that are out of time
+        cur_time = time.time()
+        for key in list(self.cache):
+            if cur_time - self.cache[key][1] >= self.timeout:
+                log_info(f"The entry \"{key}:{self.cache[key]}\" is evicted At time {cur_time}")
+                del self.cache[key]
 
 
 class Router(object):
     def __init__(self, net: switchyard.llnetbase.LLNetBase):
         self.net = net
-        # other initialization stuff here
+        self.arpCache = ARPCache()
+        # Record own ip/ethernet addr
+        for intf in self.net.interfaces():
+            self.arpCache.put(intf.ipaddr, intf.ethaddr)
 
     def handle_packet(self, recv: switchyard.llnetbase.ReceivedPacket):
         timestamp, ifaceName, packet = recv
-        # TODO: your logic here
-        ...
+        arp = packet.get_header(Arp, None)
+        log_info(f"The arp is {arp}")
+        if arp is not None:   # Handle ARP packets
+            # self-learning
+            # Use arpCache as a static table, so no need to refresh.
+            # put the sender ipaddr--etheraddr into cache
+            self.arpCache.put(arp.senderprotoaddr, arp.senderhwaddr)
+            # Forward ARP packet
+            if arp.operation == ArpOperation.Request:
+                # Search the cache to see whether the targethwaddr is already in cache
+                targethwaddr = self.arpCache.get(arp.targetprotoaddr)
+                if targethwaddr == None: # The targethwaddr is not in the cache
+                    forwardIntf = None
+                    try:
+                        forwardIntf = self.net.interface_by_ipaddr(arp.targetprotoaddr) # Get the forwarding interface.
+                        pkt = create_ip_arp_request(arp.senderhwaddr, arp.senderprotoaddr, arp.targetprotoaddr)
+                        self.net.send_packet(forwardIntf, pkt)
+                    except KeyError:
+                        # The target ipaddr is not combined to any interface, just drop it.
+                        log_info(f"The ipaddr {arp.targetprotoaddr} is not connected to the router.")
+                        pass
+                else:   # The targethwaddr is already in the cache
+                    pkt = create_ip_arp_reply(targethwaddr, arp.senderhwaddr, arp.targetprotoaddr, arp.senderprotoaddr)
+                    for forwardIntf in self.net.interfaces():
+                        if forwardIntf.name == ifaceName:
+                            self.net.send_packet(forwardIntf, pkt)
+                            break
+            elif arp.operation == ArpOperation.Reply: # Handle ARP Reply, but drop them for now according to the document
+                pass
+        else:   # Drop all kinds of other packets for now
+            pass       
+
+
 
     def start(self):
         '''A running daemon of the router.
